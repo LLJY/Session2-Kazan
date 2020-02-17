3bd7dbe696a1cf6c4f0c889927cabb43e5e98736
Lucas <lucasleeeeeeeee@gmail.com>
2020-02-17 11:36:37 +0800

    Add project files.

---
+++ Session2-Kazan/EmergencyMaintainenece.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Session2_Kazan
+{
+    public partial class EmergencyMaintainenece : Form
+    {
+        Employee LoggedInUser;
+        public EmergencyMaintainenece(Employee e)
+        {
+            LoggedInUser = e;
+            Initialize();
+        }
+        public async void Initialize()
+        {
+            var dbtask = GetData();
+            InitializeComponent();
+            dataGridView1.DataSource = await dbtask;
+        }
+        public async Task<List<MaintaineneceEmergency>> GetData()
+        {
+            var returnlist = new List<MaintaineneceEmergency>();
+            using (var db = new Session2Entities())
+            {
+                var listofassets = (from a in db.Assets
+                                where a.EmployeeID == a.EmployeeID
+                                select a).ToList();
+                Parallel.ForEach(listofassets, item=> {
+                    if ((from e in db.EmergencyMaintenances where e.AssetID == item.ID select e).Any()) {
+                        var me = new MaintaineneceEmergency();
+                        me.AssetName = item.AssetName;
+                        returnlist.Add(new MaintaineneceEmergency()
+                        {
+                            AssetName = item.AssetName,
+                            AssetSN = item.AssetSN,
+                            NumberOfEMs = (from e in db.EmergencyMaintenances where e.ID == item.ID orderby e.EMEndDate descending select e.EMEndDate).Count(),
+                            LastClosedEM = (from e in db.EmergencyMaintenances where e.ID == item.ID orderby e.EMEndDate descending select e.EMEndDate).First().ToString()
+                        }
+                        );
+                    }
+                    else
+                    {
+                        returnlist.Add(new MaintaineneceEmergency()
+                        {
+                            AssetName = item.AssetName,
+                            AssetSN = item.AssetSN,
+                            NumberOfEMs = (from e in db.EmergencyMaintenances where e.ID == item.ID orderby e.EMEndDate descending select e.EMEndDate).Count(),
+                            LastClosedEM = "---"
+                        }
+                        );
+                    }
+                });
+            }
+            return returnlist;
+        } 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //windows forms isnt fun anymore.
+            //fucking kill me
+        }
+    }
+    public class MaintaineneceEmergency
+    {
+        public string AssetSN { get; set; }
+        public string AssetName { get; set; }
+        public string LastClosedEM { get; set; }
+        public int NumberOfEMs { get; set; }
+    }
+}
---
+++ Session2-Kazan/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Session2_Kazan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.name_box = new System.Windows.Forms.TextBox();
+            this.password_box = new System.Windows.Forms.TextBox();
+            this.ok_button = new System.Windows.Forms.Button();
+            this.cancel_button = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cancel_button);
+            this.groupBox1.Controls.Add(this.ok_button);
+            this.groupBox1.Controls.Add(this.password_box);
+            this.groupBox1.Controls.Add(this.name_box);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(147, 80);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(539, 238);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sign in to EM Management";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password :";
+            // 
+            // name_box
+            // 
+            this.name_box.Location = new System.Drawing.Point(101, 61);
+            this.name_box.Name = "name_box";
+            this.name_box.Size = new System.Drawing.Size(200, 20);
+            this.name_box.TabIndex = 2;
+            this.name_box.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // password_box
+            // 
+            this.password_box.Location = new System.Drawing.Point(102, 122);
+            this.password_box.Name = "password_box";
+            this.password_box.Size = new System.Drawing.Size(200, 20);
+            this.password_box.TabIndex = 3;
+            // 
+            // ok_button
+            // 
+            this.ok_button.Location = new System.Drawing.Point(334, 190);
+            this.ok_button.Name = "ok_button";
+            this.ok_button.Size = new System.Drawing.Size(75, 23);
+            this.ok_button.TabIndex = 4;
+            this.ok_button.Text = "OK";
+            this.ok_button.UseVisualStyleBackColor = true;
+            this.ok_button.Click += new System.EventHandler(this.ok_button_Click);
+            // 
+            // cancel_button
+            // 
+            this.cancel_button.Location = new System.Drawing.Point(415, 190);
+            this.cancel_button.Name = "cancel_button";
+            this.cancel_button.Size = new System.Drawing.Size(75, 23);
+            this.cancel_button.TabIndex = 5;
+            this.cancel_button.Text = "Cancel";
+            this.cancel_button.UseVisualStyleBackColor = true;
+            this.cancel_button.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox password_box;
+        private System.Windows.Forms.TextBox name_box;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button cancel_button;
+        private System.Windows.Forms.Button ok_button;
+    }
+}
+
